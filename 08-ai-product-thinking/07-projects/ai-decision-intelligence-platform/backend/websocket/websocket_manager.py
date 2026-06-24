c281ba21f02dcdb0fb1fc85ffa5ce42544120a8c
Karthik <learner_2021@outlook.com>
2026-06-24 10:37:18 +0530

    Update websocket_manager.py

--- 08-ai-product-thinking/07-projects/ai-decision-intelligence-platform/backend/websocket/websocket_manager.py
+++ 08-ai-product-thinking/07-projects/ai-decision-intelligence-platform/backend/websocket/websocket_manager.py
@@ -1,1 +1,19 @@
+clients = []
+
+async def broadcast(data):
+
+    disconnected = []
+
+    for client in clients:
+
+        try:
+            await client.send_json(data)
+
+        except:
+            disconnected.append(client)
+
+    # cleanup dead clients
+    for d in disconnected:
+
+        clients.remove(d)
 
